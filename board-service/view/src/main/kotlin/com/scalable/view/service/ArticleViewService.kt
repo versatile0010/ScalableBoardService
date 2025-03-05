a336ad82549f41b266c3103d6b84c842db88df0a
versatile0010 <wogusn@gmail.com>
2025-03-05 23:03:29 +0900

    feat: apply distributed lock on article-view count

--- board-service/view/src/main/kotlin/com/scalable/view/service/ArticleViewService.kt
+++ board-service/view/src/main/kotlin/com/scalable/view/service/ArticleViewService.kt
@@ -1,23 +1,31 @@
 package com.scalable.view.service
 
 import com.scalable.view.repository.ArticleViewCountRepository
+import com.scalable.view.repository.ArticleViewDistributedLockRepository
 import org.springframework.stereotype.Service
+import kotlin.time.Duration.Companion.seconds
 
 @Service
 class ArticleViewService(
     private val articleViewCountRepository: ArticleViewCountRepository,
     private val articleViewCountBackUpProcessor: ArticleViewCountBackUpProcessor,
+    private val articleViewDistributedLockRepository: ArticleViewDistributedLockRepository,
 ) {
-    companion object {
-        private const val BACK_UP_BATCH_SIZE = 100
-    }
+    private val timeToLeave = 300.seconds
+    private val batchSize = 100
 
     fun increase(
         articleId: Long,
         userId: Long,
     ): Long {
+        // try lock acquire
+        if (!articleViewDistributedLockRepository.lock(articleId, userId, timeToLeave)) {
+            // if lock acquire failed, return current view count
+            return articleViewCountRepository.read(articleId)
+        }
+        // else increase view count
         val count = articleViewCountRepository.increase(articleId)
-        if (count % BACK_UP_BATCH_SIZE == 0L) {
+        if (count % batchSize == 0L) {
             articleViewCountBackUpProcessor.backUp(
                 articleId = articleId,
                 viewCount = count,
